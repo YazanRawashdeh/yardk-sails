06ea02e0ac9f5c1c27d883bddf51e80357cfe6b9
Raghad Abugharbieh <rabughar@amazon.com>
2020-11-23 13:11:41 +0200

    Add show password to login , signup and edit password pages

--- assets/js/pages/account/edit-password.page.js
+++ assets/js/pages/account/edit-password.page.js
@@ -7,11 +7,13 @@ parasails.registerPage('edit-password', {
     syncing: false,
 
     // Form data
-    formData: { /* … */ },
+    formData: {
+      showPassword: false,
+    },
 
     // For tracking client-side validation errors in our form.
     // > Has property set to `true` for each invalid property in `formData`.
-    formErrors: { /* … */ },
+    formErrors: { /* … */},
 
     // Form rules
     formRules: {
@@ -26,10 +28,10 @@ parasails.registerPage('edit-password', {
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
   //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
   //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
-  beforeMount: function() {
+  beforeMount: function () {
     //…
   },
-  mounted: async function() {
+  mounted: async function () {
     //…
   },
 
@@ -37,14 +39,15 @@ parasails.registerPage('edit-password', {
   //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
   //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
   methods: {
-
-    submittedForm: async function() {
+    togglePassword: async function () {
+      this.formData.showPassword = !this.formData.showPassword;
+    },
+    submittedForm: async function () {
       // Redirect to a different web page on success.
       // > (Note that we re-enable the syncing state here.  This is on purpose--
       // > to make sure the spinner stays there until the page navigation finishes.)
       this.syncing = true;
       window.location = '/account';
-    },
-
+    }
   }
 });
